f27176d0eae2f6484a8b083d926939530f9da453
l0rd-AK <aka883207@gmail.com>
2025-09-24 21:19:17 +0600

    refactor: remove Tailwind CSS configuration and integrate environment variables for Firebase
    - Deleted postcss.config.js and tailwind.config.js files.
    - Migrated Firebase configuration to use environment variables for better security.
    - Updated index.css to remove unnecessary border application.
    - Added new tailwind.config.cjs file with the previous configuration.
    - Introduced a new vercel.json file for routing rewrites.

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDhA21YJlFc77pNLMLdfnqdH3P-W79vxE0",
-  authDomain: "sugarcare.firebaseapp.com",
-  projectId: "sugarcare",
-  storageBucket: "sugarcare.appspot.com",
-  messagingSenderId: "165976627982",
-  appId: "1:165976627982:web:895bb2bb80d817fff9155f"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
